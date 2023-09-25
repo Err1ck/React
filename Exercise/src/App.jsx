0853894be475cb44b56e75202d9c06f73d49ff90
Err1ck <erryck-1996@hotmail.com>
2023-09-25 20:38:14 +0200

    Exercise 14

---
+++ Exercise/src/App.jsx
@@ -0,0 +1,6 @@
+import {Message} from './Exercise14'
+import {createRoot} from 'react-dom/client'
+
+createRoot(document.getElementById('root')).render(<Message/>)
+
+// Si se cambia en el index.html <script src="./src/App.jsx"> no hay problema
---
+++ Exercise/src/Exercise14.jsx
@@ -0,0 +1,7 @@
+/* 
+Can you render the Message component directly within the App component? What happens if you do?
+*/
+
+export function Message(){
+    return <h1> "What a beautiful day!" </h1>
+}
