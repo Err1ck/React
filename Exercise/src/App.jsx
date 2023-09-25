e1c6bb0782cb69ecd6d0c2835634f60ad9aabe86
Err1ck <erryck-1996@hotmail.com>
2023-09-25 19:27:17 +0200

    Exercise 8

---
+++ Exercise/src/App.jsx
@@ -0,0 +1,9 @@
+import {Hello} from "./Exercise8"
+
+export function App(){
+    return(
+        <div>
+            <Hello />
+        </div>
+    )
+}
---
+++ Exercise/src/Exercise8.jsx
@@ -0,0 +1,7 @@
+/*
+Write a function component called App that renders the Hello component within a div tag
+*/
+
+export function Hello(){
+    return <h1>Hello World</h1>
+}
