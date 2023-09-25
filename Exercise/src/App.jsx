c60ac50495440e43472e8b0173722233807a5517
Err1ck <erryck-1996@hotmail.com>
2023-09-25 19:43:37 +0200

    Exerxcise 10

---
+++ Exercise/src/App.jsx
@@ -0,0 +1,5 @@
+import {HelloExercise10} from "./Exercise10"
+
+export function App(){
+    return <HelloExercise10 />
+}    
---
+++ Exercise/src/Exercise10.jsx
@@ -0,0 +1,8 @@
+/*
+Start the application with the npm run dev command and open the application in your browser 
+(the default location is http://localhost:5173).
+*/
+
+export function HelloExercise10(){
+    return <h1>Hello World</h1>
+}
