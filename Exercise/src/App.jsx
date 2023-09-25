f6581aa13fad83ae00080bb144b07707a1705573
Err1ck <erryck-1996@hotmail.com>
2023-09-25 20:06:44 +0200

    Exercise 11

---
+++ Exercise/src/App.jsx
@@ -0,0 +1,5 @@
+import {Hello} from "./Exercise11"
+
+export function App(){
+    return <Hello />
+}
---
+++ Exercise/src/Exercise11.jsx
@@ -0,0 +1,13 @@
+/*
+Modify the Hello component so that it shows a p tag below the h2 tag with the message 
+"What a beautiful day!".
+*/
+
+export function Hello(){
+    return (
+        <div>
+            <h2>Hello World</h2>
+            <p>What a beautiful day!</p>  
+        </div>
+    )  
+}
