e51224706b27ddf7b3be3e7e358585b4231cbef7
Err1ck <erryck-1996@hotmail.com>
2023-09-25 20:23:29 +0200

    Exercise 13

---
+++ Exercise/src/App.jsx
@@ -0,0 +1,12 @@
+import {HelloExercise13} from "./Exercise13"
+
+export function App(){
+    return (
+        <div>
+            <HelloExercise13 />
+            <HelloExercise13 />
+            <HelloExercise13 />
+            <HelloExercise13 />
+        </div>
+    )
+}
---
+++ Exercise/src/Exercise13.jsx
@@ -0,0 +1,8 @@
+/*
+Can you use the Hello component more than once in the App component? 
+What happens if you do?
+*/
+
+export function HelloExercise13(){
+    return <h1>Hello World!</h1>
+}
