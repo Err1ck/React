f0f89afb6e5b7d09903b2ed34bb543701afcaf74
Err1ck <erryck-1996@hotmail.com>
2023-09-25 19:34:59 +0200

    Exercise 9

---
+++ Exercise/src/App.jsx
@@ -0,0 +1,5 @@
+import {HelloExercise9} from "./Exercise9"
+
+export function App(){
+    return <HelloExercise9/>
+}
---
+++ Exercise/src/Exercise9.jsx
@@ -0,0 +1,8 @@
+/*
+Create a new index.jsx file within the src folder and render the App 
+component using the createRoot function and the render method on the created root's object.
+*/
+
+export function HelloExercise9(){
+    return <h1>Hello World</h1>
+}
