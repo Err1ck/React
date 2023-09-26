1bde60bb0328d19cd864276028f27978a601adb2
Err1ck <erryck-1996@hotmail.com>
2023-09-26 11:44:18 +0200

    Exercise 21 (end)

--- Exercise/src/Exercise21.jsx
+++ Exercise/src/Exercise21.jsx
@@ -6,7 +6,7 @@ rendered only if the age prop is greater than 18.
 export function Welcome({age}){
     return(
         <div>
-            {age>18? <h1>Yor age is {age}</h1>: <h1>You are too young</h1>}
+            {age>18 &&<h1>Yor age is {age}</h1>}
         </div>
     )
 }
