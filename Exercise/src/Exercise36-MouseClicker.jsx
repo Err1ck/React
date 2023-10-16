65262249667a140900ccb79655e75ed3de7a2953
Err1ck <erryck-1996@hotmail.com>
2023-10-16 18:19:08 +0200

    Exercise36

--- Exercise/src/Exercise36-MouseClicker.jsx
+++ Exercise/src/Exercise36-MouseClicker.jsx
@@ -2,8 +2,10 @@
 Attach an event handler to the button that prints the name prop to the console 
 when clicked by reading the event.target.name property. */
 
-import { MouseClicker } from "./Exercise36-MouseClicker";
+export function MouseClicker({mouseName}){
+    function handleButtonClick(event){
+        console.log(event.target.name)
+    }
 
-export function App(){
-    return <MouseClicker mouseName={"ButtonName"}/>
+ return <button name={mouseName} onClick={handleButtonClick}>Click</button>   
 }
