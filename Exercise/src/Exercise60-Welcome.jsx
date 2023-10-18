2bba988f940e7852dc8772a7f49e752f8d436399
Err1ck <erryck-1996@hotmail.com>
2023-10-18 17:20:20 +0200

    Exercise60

--- Exercise/src/Exercise60-Welcome.jsx
+++ Exercise/src/Exercise60-Welcome.jsx
@@ -1,7 +1,9 @@
 /* Create an index.css file and import it within the index.jsx file. Add a .welcome class to the Welcome component that sets its background color and adds a border. */
 
-import { Welcome } from "./Exercise60-Welcome";
-
-export function App({className}){
-    return <Welcome className={className} name={"Erick"} />
+export function Welcome({name,className}){
+    return(
+        <div>
+            <h1 className={className}>Welcome {name}</h1>
+        </div>
+    )
 }
