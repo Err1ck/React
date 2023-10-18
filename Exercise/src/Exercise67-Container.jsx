bc0332b8f3feca188465cf4421bbeeedaf3f8493
Err1ck <erryck-1996@hotmail.com>
2023-10-18 20:32:37 +0200

    Exercise67

--- Exercise/src/Exercise67-Container.jsx
+++ Exercise/src/Exercise67-Container.jsx
@@ -1,11 +1,4 @@
 /* Create a Container component that renders its children within a div tag. Have the div tag use a white background and a red border, either with a custom class or by using tailwindcss. */
-import "./Exercise67-Container.css";
-import { Container } from "./Exercise67-Container";
-
-export function App() {
-  return (
-    <Container>
-      <h1>Hola</h1>
-    </Container>
-  );
+export function Container({ children }) {
+  return <div className="app">{children}</div>;
 }
