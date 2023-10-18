ea6c6722f67e03865d10ab7ad539522eebab7cf2
Err1ck <erryck-1996@hotmail.com>
2023-10-18 12:26:38 +0200

    Exercise53

--- Exercise/src/Exercise53-Colors.jsx
+++ Exercise/src/Exercise53-Colors.jsx
@@ -1,7 +1,11 @@
 /* Create a Colors component that renders a ul tag with a li tag for each color passed in the items prop. The items prop should be an array of strings. */
 
-import { Colors } from "./Exercise53-Colors";
-
-export function App() {
-  return <Colors colors={["Red", "Blue", "Green", "Grey"]} />;
+export function Colors({ colors }) {
+  return (
+    <ul>
+      {colors.map((color) => (
+        <li key={color}>{color}</li>
+      ))}
+    </ul>
+  );
 }
